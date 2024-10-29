8328d7d8d4f8785c84204c49bb9d85361a8d7b40
HJ <a01025105259@gmail.com>
2024-10-29 18:27:46 +0900

    Rename divide3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b
+    return a + b;
 }
 
 function subtract(a, b) {
@@ -7,9 +7,9 @@ function subtract(a, b) {
 }
 
 function multiply (a, b) {
-    return a * b
+    return a * b;
 }
 
-function divide(a, b) {
+function divide3(a, b) {
     return a / b
 }
